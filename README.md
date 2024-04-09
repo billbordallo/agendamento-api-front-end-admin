23fa4596768c6a51ec0a14fbf10b7552462b1d6d
Fabiano Bordallo <fabiano@treeideiasdigitais.com.br>
2024-04-09 20:33:42 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# API do sistema de agendamentos
+_versão: 1.0_
+
+Este repositório contém o microsserviço referente ao front-end de administração do sistema de agendamento do MVP para a Sprint _Desenvolvimento Back-End Avançado_, da PUC-RIO.
+
+Trata-se de um sistema de agendamento de clientes para profissionais liberais. O sistema completo engloba quatro microsserviços, cada um com seu próprio repositório.
+
+## Repositórios necessários para rodar a aplicação
+
+Para rodar o sistema completo, é necessário clonar os 4 repositórios, localizados em:
+
+- **Repositório 1** (**este repositório**) - agendamento-api-front-end-admin: é a interface que será utilizada pelo profissional liberal. Nele, é possível visualizar os agendamentos existentes, confirmar ou não a data e remover ou adicionar serviços prestados.
+
+- **Repositório 2** - [agendamento-api-back-end-admin](https://github.com/billbordallo/agendamento-api-back-end-admin): contém uma API com o back-end do sistema de administração e se comunica com o Repositório 1. Contém os bancos de dados referentes à autenticação do usuário (profissional liberal) e a tabela de serviços oferecidos.
+
+- **Repositório 3** - [agendamento-api-front-end](https://github.com/billbordallo/agendamento-api-front-end): é a interface pela qual os clientes do profissional liberal poderão realizar agendamentos. O sistema vai informar se o horário desejado está liberado ou não para agendamento.
+
+- **Repositório 4** - [agendamento-api-back-end](https://github.com/billbordallo/agendamento-api-back-end): contém uma API com o back-end do sistema de agendamentos e se comunica com o Repositório 3. Contém o banco de dados com os agendamentos realizados, informações do cliente que realizou o agendamento (nome, telefone, e-mail, endereço, serviço desejado, dia e hora desejados), bem como o status do agendamento.
+
+## Como instalar e executar este repositório usando o Docker
+
+Para rodar este repositório usando o Docker (método recomendado), após clonar o mesmo, siga os passos:
+
+1. Certifique-se de ter o [Docker](https://docs.docker.com/engine/install/) instalado e em execução em sua máquina.
+
+2. Navegue até o diretório que contém o `Dockerfile` e o `requirements.txt` no terminal.
+
+3. Execute **como administrador** o seguinte comando para construir a imagem Docker:
+
+```
+$ docker build -t agendamento-api-front-end .
+```
+
+Uma vez criada a imagem, para executar o container basta executar, **como administrador**, seguinte o comando:
+
+```
+$ docker run -p 3000:3000 agendamento-api-front-end
+```
+
+Uma vez executando, para acessar a API, basta abrir o [http://localhost:3000/#/](http://localhost:3000/#/) no navegador.
+
+## Como rodar este repositório sem usar o Docker
+
+Por se tratar de um site estático, contendo apenas HTML, CSS e Javascript, você também pode clonar o repositório em seu computador e abrir o arquivo `index.html` em seu navegador preferido.
+
+Lembre-se, no entanto, que para ter acesso a todos os recusos do projeto, é necessário ter os quatro repositórios rodando simultaneamente em seu ambiente local.
+
+## Sobre o projeto
+
+Este MVP foi desenvolvido por Fabiano Bordallo como trabalho final para a Sprint "Desenvolvimento Full Stack Avançado", da Pós-Graduação em Desenvolvimento Full Stack, do Departamento de Informática da PUC-Rio.
---
+++ scripts.js
@@ -0,0 +1,106 @@
+/*
+  --------------------------------------------------------------------------------------
+  Função para obter a lista existente do servidor via requisição GET
+  --------------------------------------------------------------------------------------
+*/
+const getList = async () => {
+    let url = 'http://127.0.0.1:5000/agendamentos';
+    fetch(url, {
+      method: 'get',
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.agendamentos.length != 0) {
+          document.getElementById("nenhum-pedido").style.display = "none";
+        }
+        data.agendamentos.forEach(item => insertList(item.id, item.nome_cliente, item.email_cliente, item.celular_cliente, item.endereco_cliente, item.data_cliente, item.hora_cliente, item.servico_cliente, item.status_agendamento))
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+  
+  /*
+    --------------------------------------------------------------------------------------
+    Chamada da função para carregamento inicial dos dados
+    --------------------------------------------------------------------------------------
+  */
+  getList()
+
+  /*
+  --------------------------------------------------------------------------------------
+  Função para inserir items na lista apresentada
+  --------------------------------------------------------------------------------------
+*/
+const insertList = (id, nome_cliente, email_cliente, celular_cliente, endereco_cliente, data_cliente, hora_cliente, servico_cliente, status_agendamento) => {
+    var item = [id, nome_cliente, email_cliente, celular_cliente, endereco_cliente, data_cliente, hora_cliente, servico_cliente, status_agendamento]
+    var table = document.getElementById('tabela-agendamentos');
+    var row = table.insertRow();
+  
+    for (var i = 0; i < item.length; i++) {
+      var cel = row.insertCell(i);
+      cel.textContent = item[i];
+      // Na sexta coluna de cada linha, adiciono a indicação para editar o status do pedido
+      if (i === 8) {
+        cel.setAttribute('title', 'Clique para editar o status do pedido');
+        cel.setAttribute('class', 'editar-status');
+      }
+    }
+    insertButton(row.insertCell(-1))
+  
+    removeElement()
+  }
+
+  /*
+  --------------------------------------------------------------------------------------
+  Função para criar um botão delete para cada item da lista
+  --------------------------------------------------------------------------------------
+*/
+const insertButton = (parent) => {
+    let span = document.createElement("span");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "delete";
+    span.appendChild(txt);
+    span.setAttribute('title', 'Remover pedido');
+    parent.appendChild(span);
+}
+
+/*
+  --------------------------------------------------------------------------------------
+  Função para remover um item da lista de acordo com o click no botão delete
+  --------------------------------------------------------------------------------------
+*/
+const removeElement = () => {
+    let deleteBt = document.getElementsByClassName("delete");
+    let i;
+    for (i = 0; i < deleteBt.length; i++) {
+      deleteBt[i].onclick = function () {
+        let div = this.parentElement.parentElement;
+        const nomeItem = div.getElementsByTagName('td')[0].innerHTML
+        if (confirm("Você tem certeza?")) {
+          div.remove()
+          deleteItem(nomeItem)
+          alert("Removido!")
+          // Atualizo a página para carregar os dados atualizados no banco de dados
+          location.reload();
+        }
+      }
+    }
+  }
+  
+  /*
+    --------------------------------------------------------------------------------------
+    Função para deletar um item da lista do servidor via requisição DELETE
+    --------------------------------------------------------------------------------------
+  */
+  const deleteItem = (item) => {
+    console.log(item)
+    let url = 'http://127.0.0.1:5000/agendamento?id=' + item;
+    fetch(url, {
+      method: 'delete'
+    })
+      .then((response) => response.json())
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
